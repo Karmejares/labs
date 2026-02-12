fc84587edc2a07671888cfba2c510763437ebff2
Nico Raigoza <nikoraigoza@gmail.com>
2026-02-12 07:04:22 -0500

    Create query_Nicolas.sql

---
+++ query_Nicolas.sql
@@ -0,0 +1,5 @@
+SELECT * FROM crime_scene WHERE location = 'Blue Note Lounge';
+SELECT * FROM suspects WHERE attire = 'trench coat' AND scar = 'left cheek';
+SELECT * FROM interviews WHERE suspect_id IN (3, 183);
+
+/* Name = Vincent Malone +/
