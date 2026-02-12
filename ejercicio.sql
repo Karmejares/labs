7e64c9759f6b5a68325e43eb1788c34e1bf9c365
Dannauc <danna.uchimacu@amigo.edu.co>
2026-02-12 07:09:43 -0500

    Create ejercicio.sql

---
+++ ejercicio.sql
@@ -0,0 +1,3 @@
+SELECT * FROM crime_scene WHERE location = "Blue Note Lounge";
+SELECT * FROM suspects WHERE attire = "trench coat" AND scar = "left cheek";
+SELECT * FROM interviews WHERE suspect_id = "3" OR suspect_id = "183"
