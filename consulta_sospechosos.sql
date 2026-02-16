9b7c5dd1d413979f37f14a8e3298f81e91c30918
James Moncada <167775428+Karmejares@users.noreply.github.com>
2026-02-16 10:26:17 -0500

    Add SQL query for suspects with specific accessories

--- consulta_sospechosos.sql
+++ consulta_sospechosos.sql
@@ -1,6 +1,14 @@
+-- case 1
 SELECT s.*, i.transcript
 FROM suspects s
 JOIN interviews i ON s.id = i.suspect_id
 WHERE s.attire = 'trench coat'
   AND s.scar = 'left cheek'
   AND i.transcript IS NOT NULL;
+-- case 2
+
+Select s.*, i.transcript 
+from suspects s 
+join interviews i on s.id = i.suspect_id 
+where bandana_color = "red"
+  and accessory = "gold watch"
